d82a85f4993e89cc3aab445d713f37751ed7872e
Nicholas Mahase <nicholasmahase@gmail.com>
2019-02-22 20:26:08 -0500

    database updates

--- app/models/user.js
+++ app/models/user.js
@@ -49,7 +49,29 @@ module.exports = function(sequelize, Sequelize) {
  
  
     });
+    
  
     return User;
+    
+    var Search = sequelize.define('search', {
  
+        id: {
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+ 
+        search: {
+            type: Sequelize.STRING,
+            notEmpty: true
+        },
+ 
+        location: {
+            type: Sequelize.STRING,
+            notEmpty: false
+        },
+ 
+    });
+ 
+    return Search;
 }

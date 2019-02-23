fdc190a5bccd879f68cc01e8c30f463dfa83aada
Nicholas Mahase <nicholasmahase@gmail.com>
2019-02-23 07:30:25 -0500

    lets try this

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -8,7 +8,7 @@ module.exports = function(app, passport) {
   app.post(
     "/signup",
     passport.authenticate("local-signup", {
-      successRedirect: "/dashboard",
+      successRedirect: "/index.html",
 
       failureRedirect: "/signup"
     })

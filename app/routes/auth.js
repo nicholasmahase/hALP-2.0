014f471f0e004e7352d953308f6d26e0a875117d
Nicholas Mahase <nicholasmahase@gmail.com>
2019-02-22 20:00:34 -0500

    page redirect

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -25,7 +25,7 @@ module.exports = function(app, passport) {
   app.post(
     "/signin",
     passport.authenticate("local-signin", {
-      successRedirect: "/dashboard",
+      successRedirect: "/index.html",
 
       failureRedirect: "/signin"
     })
